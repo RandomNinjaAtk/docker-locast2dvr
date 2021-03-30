1d7548546dcdce870d3986bdda92441858b32d09
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-03-30 19:28:51 -0400

    Create 99-start.bash

---
+++ root/etc/cont-init.d/99-start.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bash
+echo "------------------------------------------------------------"
+echo "|~) _ ._  _| _ ._ _ |\ |o._  o _ |~|_|_|"
+echo "|~\(_|| |(_|(_)| | || \||| |_|(_||~| | |<"
+echo "Presenets: locast2dvr"
+echo "------------------------------------------------------------"
+echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
+echo "Project: https://github.com/RandomNinjaAtk/docker-locast2dvr"
+echo "Support: https://discord.gg/JumQXDc"
+echo "------------------------------------------------------------"
+exit 0
